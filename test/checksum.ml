cea1509fdd711f32522d78ac4ba3ea4f1473f003
Hannes Mehnert <hannes@mehnert.org>
2023-11-22 21:05:55 +0100

    test more checksums, also with unaligned data (cstruct.off <> 0)

--- test/checksum.ml
+++ test/checksum.ml
@@ -66,6 +66,53 @@ let real1 () =
   in
   Alcotest.(check int "real1" 0x8399 Segment.(checksum ~src ~dst data))
 
+let real2 () =
+  let data = Cstruct.of_hex {|
+95 fa 00 50 37 46 1d 2e  00 00 00 00 a0 02 ff ff
+00 00 00 00 02 04 05 b4  01 03 03 06 04 02 08 0a
+89 8e 28 be 00 00 00 00|}
+  in
+  let src = Ipaddr.(V4 (V4.of_string_exn "10.0.42.1"))
+  and dst = Ipaddr.(V4 (V4.of_string_exn "10.0.42.2"))
+  in
+  Alcotest.(check int "real2" 0x42f3 Segment.(checksum ~src ~dst data))
+
+let real3 () =
+  let data = Cstruct.of_hex {|
+95 fa 00 50 37 46 1d 2e  00 00 00 00 a0 02 ff ff
+00 00 00 00 02 04 05 b4  01 03 03 06 04 02 08 0a
+89 8e 28 be 00 00 00 00|}
+  in
+  let data = Cstruct.(shift (append (create 25) data) 25) in
+  let src = Ipaddr.(V4 (V4.of_string_exn "10.0.42.1"))
+  and dst = Ipaddr.(V4 (V4.of_string_exn "10.0.42.2"))
+  in
+  Alcotest.(check int "real3" 0x42f3 Segment.(checksum ~src ~dst data))
+
+let real4 () =
+  let data = Cstruct.of_hex {|
+95 fa 00 50 37 46 1d 2e  00 00 00 00 a0 02 ff ff
+00 00 00 00 02 04 05 b4  01 03 03 06 04 02 08 0a
+89 8e 28 be 00 00 00 00  02 02 02|}
+  in
+  let data = Cstruct.(shift (append (create 25) data) 25) in
+  let src = Ipaddr.(V4 (V4.of_string_exn "10.0.42.1"))
+  and dst = Ipaddr.(V4 (V4.of_string_exn "10.0.42.2"))
+  in
+  Alcotest.(check int "real4" 16110 Segment.(checksum ~src ~dst data))
+
+let real5 () =
+  let data = Cstruct.of_hex {|
+95 fa 00 50 37 46 1d 2e  00 00 00 00 a0 02 ff ff
+00 00 00 00 02 04 05 b4  01 03 03 06 04 02 08 0a
+89 8e 28 be 00 00 00 00  02 02|}
+  in
+  let data = Cstruct.(shift (append (create 25) data) 25) in
+  let src = Ipaddr.(V4 (V4.of_string_exn "10.0.42.1"))
+  and dst = Ipaddr.(V4 (V4.of_string_exn "10.0.42.2"))
+  in
+  Alcotest.(check int "real5" 16623 Segment.(checksum ~src ~dst data))
+
 let tests = [
   "simple", `Quick, simple ;
   "data1", `Quick, data1 ;
@@ -74,4 +121,8 @@ let tests = [
   "data4", `Quick, data4 ;
   "data5", `Quick, data5 ;
   "real1", `Quick, real1 ;
+  "real2", `Quick, real2 ;
+  "real3", `Quick, real3 ;
+  "real4", `Quick, real4 ;
+  "real5", `Quick, real5 ;
 ]
