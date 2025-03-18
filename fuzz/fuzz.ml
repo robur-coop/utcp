ef871920cc167ec321a664982169e522d72ff75c
Calascibetta Romain <romain.calascibetta@gmail.com>
2025-03-18 14:15:04 +0100

    Use bytes instead of cstruct to construct pseudoheader and compute checksum

--- fuzz/fuzz.ml
+++ fuzz/fuzz.ml
@@ -6,6 +6,8 @@ let pp ppf = Format.fprintf ppf "%04x"
 let () =
   add_test ~name:"checksum" [ buf ] @@ fun buf ->
   let { Cstruct.buffer; off; len } = Cstruct.of_string buf in
-  let a = Utcp.Checksum.unsafe_digest_16_le ~off ~len buffer in
-  let b = Utcp.Checksum.unsafe_digest_32_le ~off ~len buffer in
+  let a = Utcp.Checksum.unsafe_feed_16_le ~off ~len 0 buffer in
+  let b = Utcp.Checksum.unsafe_feed_32_le ~off ~len 0 buffer in
+  let a = Utcp.Checksum.finally a in
+  let b = Utcp.Checksum.finally b in
   check_eq ~pp a b
